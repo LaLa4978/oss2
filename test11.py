9624b325a3726eb8e25abd70ab684bd2b62a0eec
LaLa4978 <kirby497878@gmail.com>
2024-12-01 20:32:15 +0900

    Add new file

--- test11.py
+++ test11.py
@@ -1,7 +1,6 @@
 import sympy as sp
-#굿
 
-def function():
+def taylor_series():
     formula_input = input("함수 입력: ")
     x = sp.Symbol('x')
     formula = sp.sympify(formula_input)
@@ -9,5 +8,3 @@ def function():
 
     taylor = sp.series(formula, x, 0, 6)
     print("테일러 급수: {}". format(taylor))
-
-function()
